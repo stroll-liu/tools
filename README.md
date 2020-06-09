d76833cc5c29cd60904a28a3d124fbd1df83bbfd
liuyi <liuyi@hetao101.com>
2020-06-10 01:23:58 +0800

    tools

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # tools
 工具类
+
+## 安装
+
+```js
+npm i @stroll/tools
+
+```
+## 引入
+```js
+import Vue from 'vue'
+import Tools from '@stroll/tools'
+
+Vue.prototype.$tools = Tools
+
+```
+## 调用
+```js
+// 字段排查，如果有就返回，没有返回自定义值 或 默认值（''）
+// obj 比传参数
+// replaceVal 可选参数，自定义值（默认 ''）
+// exec 可选参数，截至符（默认 _）
+ask(obj, replaceVal, exec).a.b.c._
+
+// 数字转汉字
+// num 比传参数,需要转换的数字
+DTC(num)
+
+// 简易提示
+// str 提示文案，默认 '未知错误',
+// t显示时间，单位毫秒，默认 2000,
+// tipStyle 提示框样式,
+// fn 提示框消失后的回调
+textTip({ str, t, tipStyle, fn })
+
+// 获取操作系统
+detectOS()
+
+// 参数过滤
+// 只传第一个参，返回参数的所有key
+// 传两个参数，把第二个参数合并到第一个里
+// 第三个参数，第二个参数需要过滤调的key
+// 前两个参数为json对象，第三个为数组
+jsonTreat(jsonObjA, jsonObjB, arr)
+
+// 过滤
+// v 需要过滤的值
+// 过滤的 规则 （目前只有 'onlyNumbers'）
+fromVerify(v, format)
+
+// 设置session
+// name 参数 session 的 key
+// obj 参数 session 的 value
+setSession({ name }, obj)
+
+// 获取session
+// name 参数 session 的 key
+getSession({ name })
+
+// 更新session
+// name 参数 session 的 key
+// arr 参数 需要更新的 key
+// obj 参数 需要更新的
+pushSession({ name, arr }, obj = {})
+
+```
---
+++ index.js
@@ -0,0 +1,214 @@
+// 字段排查
+export async function ask (obj, replaceVal = '', exec = '_') {
+  return new Proxy({}, {
+    get: (target, key) => {
+      if (key === exec) {
+        return obj || replaceVal
+      } else {
+        return ask(!!obj && obj[key], replaceVal, exec)
+      }
+    }
+  })
+}
+
+// 数字转汉字
+export async function DTC (num) {
+  let chnNumChar = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']
+  let chnUnitSection = ['', '万', '亿', '万亿', '亿亿']
+  let chnUnitChar = ['', '十', '百', '千']
+
+  function SectionToChinese (section) {
+    let strIns = ''
+    let chnStr = ''
+    let unitPos = 0
+    let zero = true
+    while (section > 0) {
+      let v = section % 10
+      if (v === 0) {
+        if (!zero) {
+          zero = true
+          chnStr = chnNumChar[v] + chnStr
+        }
+      } else {
+        zero = false
+        strIns = chnNumChar[v]
+        strIns += chnUnitChar[unitPos]
+        chnStr = strIns + chnStr
+      }
+      unitPos++
+      section = Math.floor(section / 10)
+    }
+    return chnStr
+  }
+
+  let unitPos = 0
+  let strIns = ''
+  let chnStr = ''
+  let needZero = false
+
+  if (num === 0) {
+    return chnNumChar[0]
+  }
+
+  while (num > 0) {
+    let section = num % 10000
+    if (needZero) {
+      chnStr = chnNumChar[0] + chnStr
+    }
+    strIns = SectionToChinese(section)
+    strIns += (section !== 0) ? chnUnitSection[unitPos] : chnUnitSection[0]
+    chnStr = strIns + chnStr
+    needZero = (section < 1000) && (section > 0)
+    num = Math.floor(num / 10000)
+    unitPos++
+  }
+  return chnStr
+}
+
+// 去重
+export async function dedupe (arr) {
+  return [...new Set(arr)]
+}
+
+// 简易提示
+let textTipShow = false
+export async function textTip ({ str = '未知错误', t = 2000, tipStyle, fn }) {
+  if (textTipShow) return
+  tipStyle = tipStyle || `
+      background:rgba(0,0,0,.3); color: #fff; border-radius: 3px; padding: 20px 10px;
+      line-height: 40px; text-align: center; font-size: 30px;
+      position: fixed; left: 50%; top: 20%; z-index: 9999;
+      -webkit-transform: translate(-50%); transform: translate(-50%);`
+  textTipShow = true
+  var dom = document.createElement('div')
+  dom.setAttribute('style', tipStyle)
+  dom.innerHTML = str
+
+  document.body.appendChild(dom)
+
+  if ((dom.offsetHeight - 20) / 18 > 1) {
+    dom.style.width = '55%'
+  }
+
+  setTimeout(function () {
+    dom.parentNode.removeChild(dom)
+    textTipShow = false
+    if (fn) { fn() }
+  }, t)
+}
+
+// 获取操作系统
+export async function detectOS () {
+  const sUserAgent = navigator.userAgent
+  if (['Win32', 'Windows'].includes(navigator.platform)) {
+    if (sUserAgent.includes('Windows NT 5.0') || sUserAgent.includes('Windows 2000')) {
+      return 'Windows2000'
+    } else if (sUserAgent.includes('Windows NT 5.1') || sUserAgent.includes('Windows XP')) {
+      return 'WindowsXP'
+    } else if (sUserAgent.includes('Windows NT 5.2') || sUserAgent.includes('Windows 2003')) {
+      return 'Windows2003'
+    } else if (sUserAgent.includes('Windows NT 6.0') || sUserAgent.includes('Windows Vista')) {
+      return 'Windows Vista'
+    } else if (sUserAgent.includes('Windows NT 6.1') || sUserAgent.includes('Windows 7')) {
+      return 'Windows7'
+    } else if (sUserAgent.includes('Windows NT 10.0') || sUserAgent.includes('Windows 10')) {
+      return 'Windows10'
+    } else {
+      return 'Windows other'
+    }
+  } else if (['Mac68K', 'MacPPC', 'Macintosh', 'MacIntel'].includes(navigator.platform)) {
+    return 'MacOS'
+  } else if (navigator.platform === 'X11') {
+    return 'Unix'
+  } else if (String(navigator.platform).includes('Linux')) {
+    return 'Linux'
+  } else {
+    return 'other'
+  }
+}
+
+// 参数过滤
+export async function jsonTreat (a, b, arr) {
+  let arr = []
+  if (b) {
+    Object.keys(b).forEach(key => {
+      if (arr) {
+        if (!arr.includes(key)) {
+          a[key] = b[key]
+        }
+      } else {
+        a[key] = b[key]
+      }
+      arr.push(key)
+    })
+  } else if (a) {
+    Object.keys(a).forEach(key => {
+      if (a[key]) {
+        arr.push(key)
+      }
+    })
+  }
+  return arr
+}
+
+// 过滤
+export async function fromVerify(v, format) {
+  if (format === 'onlyNumbers') {
+    return String(v).replace(/[^\d]/g, '')
+  }
+}
+
+// 设置session
+export async function setSession ({ name }, obj) {
+  return sessionStorage.setItem(name, obj)
+}
+
+// 获取session
+export async function getSession ({ name }) {
+  return sessionStorage.getItem(name)
+}
+
+// 更新session
+export async function pushSession ({ name, arr }, obj = {}) {
+  const msg = getSession({ name: name })
+  let msgKey = null
+  let index = 0
+  
+  async function getMsgKey (item) {
+    if (arr.length > index) {
+      if (item[arr[index]]) {
+        msgKey = item[arr[index]]
+      } else {
+        console.log(`没有 ${arr[index]} 集合`)
+        return
+      }
+      index++
+      await getMsgKey(msgKey)
+    }
+  }
+  if (arr && arr.length) {
+    await getMsgKey(item)
+    for (let key in obj) {
+      msgKey[key] = obj[key]
+    }
+  } else {
+    for (let key in obj) {
+      msg[key] = obj[key]
+    }
+  }
+
+  return setSession({ name: name }, msg)
+}
+
+export default {
+  ask,
+  DTC,
+  dedupe,
+  textTip,
+  detectOS,
+  jsonFilter,
+  fromVerify,
+  setSession,
+  getSession,
+  pushSession
+}
