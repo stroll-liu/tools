a4d148043e12f394438833c912d4e27fba95756b
liuyi <13403111884@163.com>
2020-06-16 17:00:31 +0800

    去除 excel 相关

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Vue.prototype.$tools = Tools
 ## 调用
 ```js
 // 字段排查，如果有就返回，没有返回自定义值 或 默认值（''）
-// obj 比传参数
+// obj 排查的json
 // replaceVal 可选参数，自定义值（默认 ''）
 // exec 可选参数，截至符（默认 _）
 ask(obj, replaceVal, exec).a.b.c._
@@ -65,23 +65,5 @@ getSession({ name })
 // arr 参数 需要更新的 key
 // obj 参数 需要更新的
 pushSession({ name, arr }, obj = {})
-
-// 导出 csv
-// tHeader 数组, 表头
-// filterKey 数组， 需要导出的字段key
-// list 数组， 需要导出的数组
-exportCSV({tHeader, filterKey, list})
-
-// 导出 Excel
-// tableJson 数组，表设置 （支持多sheet）示例：[{header, multiHeader, merges, filterKey, list, sheetName}, {header, multiHeader, merges, filterKey, list, sheetName}]
-  // header 数组，表头 示例：["序号", "标题", "作者", "服务"]
-  // multiHeader 数组，二级表头，同 header
-  // merges 数组， 合并单元格 示例：["A1:A2", "B1:B2", "C1:E1"]
-  // filterKey 数组，过滤需要展示的字段 示例：["id", "title", "author", "reviewer"]
-  // list 数组， 展示的数据 示例： [{...}, {...}, {...}]
-  // sheetName 字符串，sheet名称
-// filename 字符串，文件名称 默认 `${随机字符串}-${当前时间戳}`
-// bookType 字符串，文件格式 默认 xlsx
-exportExcel({ tableJson, filename, bookType })
 
 ```
--- index.js
+++ index.js
@@ -203,58 +203,6 @@ export async function pushSession ({ name, arr }, obj = {}) {
   return setSession({ name: name }, msg)
 }
 
-// 导出 csv
-export async function exportCSV ({ header = [], filterKey = [], list = [] }) {
-  const mainStr = []
-  mainStr.push(header.join("\t,")+"\n")
-  list.forEach( item => {
-    const temp = []
-    filterKey.forEach(el => {
-      temp.push(item[el])
-    })
-    mainStr.push(temp.join("\t,")+"\n")
-  })
-
-  const uri = 'data:text/csv;charset=utf-8,\ufeff' + encodeURIComponent(mainStr.join(""))
-  let link = document.createElement('a')
-  link.href = uri
-  link.download = `${Math.random().toString(36).slice(-8)}-${new Date().getTime()}.csv`
-  document.body.appendChild(link)
-  link.click()
-}
-
-// 导出 excel
-import { export_json_to_excel } from './core/Export2Excel'
-export async function exportExcel ({ tableJson = [], filename, bookType }) {
-  function formatJson (filterKey, jsonData) {
-    return jsonData.map(v => filterKey.map(j => v[j]))
-  }
-
-  const header = []
-  const data = []
-  const sheetname = []
-  const multiHeader = []
-  const merges = []
-
-  tableJson.forEach((item, i) => {
-    header.push(item.header || item.filterKey || [])
-    merges.push(item.merges || [])
-    multiHeader.push(item.multiHeader || [])
-    data.push(formatJson(item.filterKey, item.list || []))
-    sheetname.push(item.sheetName || `${Math.random().toString(36).slice(-8)}-${new Date().getTime()}-${i}`)
-  })
-
-  export_json_to_excel({
-    multiHeader,
-    header,
-    data,
-    sheetname,
-    merges,
-    bookType,
-    filename: filename || `${Math.random().toString(36).slice(-8)}-${new Date().getTime()}`
-  })
-}
-
 export default {
   ask,
   DTC,
@@ -265,7 +213,5 @@ export default {
   randomString,
   setSession,
   getSession,
-  pushSession,
-  exportCSV,
-  exportExcel
+  pushSession
 }
