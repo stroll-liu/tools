c117568d941c51cb8884abdcabdcc5e806351354
13403111884 <42490122+13403111884@users.noreply.github.com>
2021-12-14 20:10:17 +0800

    Create ask.js

---
+++ ask.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: 'a',
+    type: 'input',
+    description: '参数一'
+  },
+  {
+    name: 'b',
+    type: 'input',
+    description: '参数二'
+  },
+  {
+    name: 'c',
+    type: 'input',
+    description: '参数三'
+  }
+];
